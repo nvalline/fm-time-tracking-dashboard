dd13c42509215469bebce405852d82b1d3c97ad6
vall4251n <58278138+nvalline@users.noreply.github.com>
2024-05-28 14:12:48 -0600

    import data & add link structure

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+import data from './data.json' with{ type: 'json' };
 
+console.log(data);
+
+const filterTabs = document.querySelectorAll('.filter');
+
+for(let i = 0; i < filterTabs.length; i++) {
+	filterTabs[i].addEventListener('click', (e) => {
+		console.log('HIT', e.target);
+	});
+}
