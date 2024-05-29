2cd6d3608c82563507925e3e198e0f152fe4ba4a
vall4251n <58278138+nvalline@users.noreply.github.com>
2024-05-29 14:25:15 -0600

    updated script for tabs and data

--- script.js
+++ script.js
@@ -2,10 +2,80 @@ import data from './data.json' with{ type: 'json' };
 
 console.log(data);
 
-const filterTabs = document.querySelectorAll('.filter');
+// ----- Data Handling -----
 
-for(let i = 0; i < filterTabs.length; i++) {
-	filterTabs[i].addEventListener('click', (e) => {
-		console.log('HIT', e.target);
-	});
+const panelList = document.getElementById('panelList')
+
+const setPanel = (timeframe) => {
+	
+	data.forEach(category => {
+		// console.log('TITLE', category.title)
+		// console.log('TIMEFRAME', category.timeframes)
+		const li = document.createElement('li')
+	
+		li.innerHTML = `
+				<li id="work" class="card">
+					<img
+						src="./images/icon-work.svg"
+						alt=""
+						height="78"
+						width="78"
+						class="icon"
+					/>
+					<div class="content">
+						<div class="category">
+							<h2>Work</h2>
+							<img src="./images/icon-ellipsis.svg" alt="" />
+						</div>
+						<div id="workData" class="data">
+							<h3><span>32</span>hrs</h3>
+							<p>Last Week - <span>36</span>hrs</p>
+						</div>
+					</div>
+				</li>
+			`;
+
+		panelList.appendChild(li);
+	})
+
 }
+
+// ----- Tabbed Interface -----
+
+const tablist = document.querySelector('.tablist')
+const tabs = document.querySelectorAll('.tab');
+
+// The tab switching function
+const switchTab = (oldTab, newTab) => {
+	newTab.focus()
+	// Make the active tab focusable by the user (Tab key)
+	newTab.removeAttribute('tabindex')
+	// Set the selected state
+	newTab.setAttribute('aria-selected', 'true')
+	oldTab.removeAttribute('aria-selected')
+	oldTab.setAttribute('tabindex', '-1')
+	// Get the timeframe of the new and old tabs to find the correct tab panels to show and hide
+	let timeframe = newTab.getAttribute('id')
+	
+	setPanel(timeframe)
+}
+
+// Add semantics to remove user focusability for each tab
+Array.prototype.forEach.call(tabs, (tab, i) => {
+	tab.setAttribute('tabindex', '-1')
+
+	// Handle mouse user clicks
+	tab.addEventListener('click', (e) => {
+		e.preventDefault()
+		let currentTab = tablist.querySelector('[aria-selected]')
+
+		if(e.currentTarget !== currentTab) {
+			switchTab(currentTab, e.currentTarget)
+		}
+	})
+})
+
+// Initially activate the first tab and reveal the first tab panel
+tabs[1].removeAttribute('tabindex');
+tabs[1].setAttribute('aria-selected', 'true');
+setPanel(tabs[1].id);
