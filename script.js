dd1eb9a8f8c7b6a81be94e71588cb38de56e7f40
vall4251n <58278138+nvalline@users.noreply.github.com>
2024-06-10 13:35:41 -0600

    added card button aria label

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ const setPanel = (timeframe) => {
 					<div class="content">
 						<div class="category">
 							<h2>${category.title}</h2>
-							<button class="cardButton">
+							<button aria-label="card button" class="cardButton">
 								<svg width="21" height="5" xmlns="http://www.w3.org/2000/svg">
 									<path d="M2.5 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Z" fill="#BBC0FF" fill-rule="evenodd"/>
 								</svg>
